cc954fe0bf6bf247e2907a3ab879b9f6f3721bc0
crparker7 <60588229+crparker7@users.noreply.github.com>
2023-10-31 12:34:23 -0400

    screenshot of results from first run

--- read_tif.py
+++ read_tif.py
@@ -1,6 +1,6 @@
-from PIL import Image
-from PIL.TiffTags import TAGS
-
+import utils_6521 as utils
+import numpy as np
+import torch
 # (220, 140, 0) is the RGB value of a building pixel in GTC files.
 # 6 represents a building pixel in GTL files.
 # (224, 224, 224) is a background pixel in GTC files.
@@ -10,8 +10,20 @@ from PIL.TiffTags import TAGS
 
 # See https://www.topcoder.com/challenges/db36b53a-c2f3-4899-9698-13e96148ffcd for more information.
 
-with Image.open('Datasets/Urban_3D_Challenge/01-Provisional_Train/GT/JAX_Tile_004_GTL.tif') as img:
-    meta_dict = {TAGS[key] : img.tag[key] for key in img.tag.keys()}
-    print(list(img.getdata()))
+truth_image = utils.get_truth_image('Datasets/Urban_3D_Challenge/01-Provisional_Train/GT/JAX_Tile_004_GTI.tif', 2048, 2048)
+print('Truth image shape:', truth_image.shape)
 
-print(meta_dict.keys())
+num_buildings = truth_image.max()
+
+a1 = [(10, 20, 30, 20), (20, 10, 15, 10), (10, 15, 40, 50), (20, 5, 30, 30)]
+
+a2 = sorted(a1)
+
+print(a2)
+# for i in range(num_buildings + 1):
+#     building_mask = np.where(truth_image==i, 1, 0)
+#     building_mask_tensor = torch.from_numpy(building_mask)
+#     arr = np.nonzero(building_mask)
+#     print('First el = ', arr[0][0], arr[1][0])
+#     print('Last el = ', arr[0][-1], arr[1][-1])
+
