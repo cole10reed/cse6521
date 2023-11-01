Bug fix in test_sam for building bbox calculation

test_sam.py
@@ -1,7 +1,7 @@
 from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
 import cv2
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import torch
 from torchmetrics import JaccardIndex
 import utils_6521 as utils
@@ -9,7 +9,7 @@ from pycocotools import mask as mask_utils
 from multiprocessing import Pool
 import time
 
-'''
+
 def show_anns(anns):
     if len(anns) == 0:
         return
@@ -20,12 +20,12 @@ def show_anns(anns):
     img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
     img[:,:,3] = 0
     for ann in sorted_anns:
-        print(ann)
+        # print(ann)
         m = ann['segmentation']
         color_mask = np.concatenate([np.random.random(3), [0.35]])
         img[m] = color_mask
     ax.imshow(img)
-'''
+
 
 def exe(sam_checkpoint ='Segment-Anything/checkpoints/sam_vit_h_4b8939.pth', device = 'cuda', model_type = 'vit_h', dataset_loc = r"C:\Users\Micha\Documents\CSE 6521\Datasets"):
 
@@ -74,8 +74,8 @@ def exe(sam_checkpoint ='Segment-Anything/checkpoints/sam_vit_h_4b8939.pth', dev
     true_pos = list()
 
     masks = sorted(masks, key=(lambda x: x['bbox']))
-    input_masks = [torch.from_numpy(x['segmentation']) for x in masks]
-    # truth_masks = [torch.from_numpy(np.where(truth_image==i, True, False)) for i in range(num_buildings+1)]
+    # input_masks = [torch.from_numpy(x['segmentation']) for x in masks]
+
     print("Starting outer for loop")
 
     nbreaks = 0
@@ -85,22 +85,18 @@ def exe(sam_checkpoint ='Segment-Anything/checkpoints/sam_vit_h_4b8939.pth', dev
         building_mask = np.where(truth_image==i, 1, 0)
         building_mask_tensor = torch.from_numpy(building_mask)
         arr = np.nonzero(building_mask)
-        # print('First el = ', arr[0][0], arr[1][0])
-        # print('Last el = ', arr[0][-1], arr[1][-1])
         
         # Building bbox is as follows (least x value, greatest x val, least y value, greatest y val)
         # Forms a box with four corners (bx1,by1), (bx1, by2), (bx2, by1), (bx2, by2)
-        bx1 = min(arr[0])
-        bx2 = max(arr[0])
-        by1 = min(arr[1])
-        by2 = max(arr[1])
+        by1 = min(arr[0])
+        by2 = max(arr[0])
+        bx1 = min(arr[1])
+        bx2 = max(arr[1])
 
         for j in range(len(masks)):
-            # print('Shape of annotation: ', masks[j]['segmentation'].shape)
-            # print('Shape of building mask: ', building_mask.shape)
-            x,y,w,h = masks[j]['bbox']
-            
-            ''' 
+            x,y,h,w = masks[j]['bbox']
+            # print('x,y', x,y)
+            '''
             # Old calculation (works, but takes ~35 min)
             if ((x > arr[0][-1]) and (y > arr[1][-1])):
                 nbreaks += 1 # First point of mask occurs after last point of building, no further masks will intersect.
@@ -108,36 +104,48 @@ def exe(sam_checkpoint ='Segment-Anything/checkpoints/sam_vit_h_4b8939.pth', dev
             if ((x + w < arr[0][0]) and (y + h < arr[1][0])):
                 ncontinue += 1 # Last point of mask occurs before first point of building, keep looking but skip this mask.
                 continue
-
-            ''' 
+            '''
+             
 
             '''
             Current effort (broken, but fast!)
             '''
             
             if (x > bx2):   # Masks are past the building, no more possible intersections.
-                print('BREAKING')
-                print('mask = ',x,y,x+w,y+h)
-                print('bldg = ',bx1,by1,bx2,by2)
+                # print('BREAKING')
+                # print('mask = ',x,y,x+w,y+h)
+                # print('bldg = ',bx1,by1,bx2,by2)
                 nbreaks += 1
                 break
-            if (((y+h) < by1) or (y > by2) or ((x+w) < bx1)): # Mask lies outside building bbox, keep looking but skip this iteration. 
-                print('CONTINUING')
-                print('mask = ',x,y,x+w,y+h)
-                print('bldg = ',bx1,by1,bx2,by2)
+            # if (((y+h) < by1) or (y > by2) or ((x+w) < bx1)): # Mask lies outside building bbox, keep looking but skip this iteration. 
+                # print('CONTINUING')
+                # print('mask = ',x,y,x+w,y+h)
+                # print('bldg = ',bx1,by1,bx2,by2)
+            #     ncontinue += 1
+            #     continue
+            
+            if (y+h < by1):
                 ncontinue += 1
                 continue
+            if (y > by2):
+                ncontinue += 1
+                continue
+            if (x+w < bx1):
+                ncontinue += 1
+                continue
+            # print(i, j)
             
-            print(i, j)
-            
-
-            segment = input_masks[j]
+            segment = torch.from_numpy(masks[j]['segmentation'])
+            # segment = input_masks[j]
             res = jaccard(building_mask_tensor, segment)
             if (res >= 0.45):
                 true_pos.append(i)
                 print('-------- FOUND MATCH --------')
-                print("bx1: ", bx1, " by1: ", by1, " bx2: ", bx2, " by2: ", by2, " x: ", x, " y: ", y, " h: ", h, " w: ", w)
+                print("bx1: ", bx1, " bx2: ", bx2, " by1: ", by1, " by2: ", by2)
+                print(" x1: ", x, "  x2: ", x+w, "  y1: ", y, "  y2: ", y+h)
+                print(segment[x:x+w+1,y:y+h+1])
                 del(masks[j])
+                # del(input_masks[j])
                 break
 
     end = time.time()
@@ -148,11 +156,14 @@ def exe(sam_checkpoint ='Segment-Anything/checkpoints/sam_vit_h_4b8939.pth', dev
     print('Number of breaks', nbreaks)
     print('Number of continues', ncontinue)
 
-    '''
+    
     plt.figure(figsize=(20,20))
     plt.imshow(image)
     show_anns(masks)
     plt.axis('off')
     plt.savefig(fname='test_128pps')
     plt.show()
-    '''
+    
+
+if __name__=='__main__':
+    exe(dataset_loc='Datasets/Urban_3D_Challenge')